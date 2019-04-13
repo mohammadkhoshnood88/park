65cc173adead9eee6df0eb6d78385e83fab67afc
mohammadkhoshnood <mohammadkhoshnood88@gmail.com>
2019-04-13 20:12:42 +0430

    add123

---
+++ database/migrations/2019_03_14_092404_create_races_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRacesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('races', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('mac_address');
+            $table->string('beacon_mac');
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('races');
+    }
+}
